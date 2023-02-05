8fa9972f33f78dd103fbabc82a723864ad7ff075
Lucas Reis <lucasgarciareiss@gmail.com>
2023-02-05 20:56:59 -0300

    Helpers

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -16,9 +16,27 @@ describe.only('Cypress basics', () => {
         //     .should('be.equal', 'Campo de Treinamento')
         //     .and('contain', 'Campo')
 
-            cy.title().then(title => {
-                console.log(title)
-            })
+        cy.title().then(title => {
+            console.log(title)
+        })
+
+        let syncTitle
+
+        cy.title('#formNome').then(title => {
+            console.log(title)
+
+            cy.get('#formNome').type(title)
+            syncTitle = title
+        })
+
+        cy.get('[data-cy="dataSobrenome"]').then($el => {
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
+        })
+
     })
 })
 
