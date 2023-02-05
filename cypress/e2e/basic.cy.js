993a0f2ff624f63dc82dd509446330a7f88f7dd8
Lucas Reis <lucasgarciareiss@gmail.com>
2023-02-05 15:15:16 -0300

    Sync

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,20 +1,24 @@
 /// <reference types="cypress"/>
 
 describe.only('Cypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         // const title = cy.title();
         // console.log(title);
 
-        cy.pause();
+        // cy.pause();
 
-        cy.title().should('be.equal', 'Campo de Treinamento')
-        cy.title().should('contain', 'Campo');
+        // cy.title().should('be.equal', 'Campo de Treinamento')
+        // cy.title().should('contain', 'Campo');
 
-        cy.title()
-            .should('be.equal', 'Campo de Treinamento')
-            .and('contain', 'Campo')
+        // cy.title()
+        //     .should('be.equal', 'Campo de Treinamento')
+        //     .and('contain', 'Campo')
+
+            cy.title().then(title => {
+                console.log(title)
+            })
     })
 })
 
